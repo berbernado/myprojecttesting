46f2ece8601aa9b2257ee0ba7dd8446e4b92d456
berbernado <bernadoanthosoeryo@gmail.com>
2019-08-21 20:43:54 +0700

    3 files modified, 10 added, 10 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { ViewModule } from './view/view.module';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [ 
+    BrowserModule, 
+    FormsModule,
+    ViewModule
+  ],
+  declarations: [ 
+    AppComponent,
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
--- src/app/view/view.module.ts
+++ src/app/view/view.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LayoutComponent } from './layout/layout.component';
+import { MainComponent } from './main/main.component';
 import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';'
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [
