824486839c81d55f52c85036b17b3c4402669298
berbernado <bernadoanthosoeryo@gmail.com>
2019-08-21 21:10:47 +0700

    2 files modified, 3 added, 3 deleted

--- src/app/view/view.module.ts
+++ src/app/view/view.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MainComponent } from './main/main.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { BodyComponent } from './body/body.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [HeaderComponent, BodyComponent, FooterComponent],
+  exports: [BodyComponent]
 })
 export class ViewModule { }
