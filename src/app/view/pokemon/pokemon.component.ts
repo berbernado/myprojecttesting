762d1a1e955feb17d77e8fe9f1f271d89a1fe0df
berbernado <bernadoanthosoeryo@gmail.com>
2019-08-21 22:34:17 +0700

    1 binaries uploaded

--- src/app/view/pokemon/pokemon.component.ts
+++ src/app/view/pokemon/pokemon.component.ts
@@ -12,4 +12,23 @@ export class PokemonComponent implements OnInit {
   ngOnInit() {
   }
 
+  getAllInnovation() {
+    this.loadingawal = true;
+    this.httpService.get<any>(this.IPURL + UrlCollection.GETPROGRESINNOVATION + this._myconfig.getNip(),
+      {
+        headers:{
+          'content-Type': 'application/json',
+          'Authorization': 'Bearer ' + this._myconfig.getToken()
+        }
+      }).subscribe(resInno => {
+      this.arrInnovation = resInno ;
+      this.loadingawal = false;
+    },
+      error => {
+        if (error.status === '401') {
+          this._myconfig.logoutService();
+      }
+    });
+  }
+
 }
