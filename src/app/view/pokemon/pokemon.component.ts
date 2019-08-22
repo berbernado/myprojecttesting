0ac5dbbb91eed5818432b58ce4be1d940736e962
berbernado <bernadoanthosoeryo@gmail.com>
2019-08-22 14:22:54 +0700

    2 files modified

--- src/app/view/pokemon/pokemon.component.ts
+++ src/app/view/pokemon/pokemon.component.ts
@@ -9,8 +9,8 @@ import { UrlCollection } from '../../../config/UrlCollection';
 })
 export class PokemonComponent implements OnInit {
   
-  public arrPoke: any;
-  loadimg = true;
+  public arrPoke: [];
+  public listpoke = [];
   loading = false;
   constructor(private httpService: HttpClient) { }
 
@@ -26,7 +26,18 @@ export class PokemonComponent implements OnInit {
           'content-Type': 'application/json',
         }
       }).subscribe(data => {
-      this.arrPoke = data.results ;
+      this.arrPoke = data.results;
+      let i = 0;
+      for (const result of this.arrPoke) {
+           i = i + 1;
+           let urlphoto = this.getPokeImages(i);
+          this.listpoke.push({
+              id: i,
+              nama: result.name,
+              foto: urlphoto
+          });
+        }
+      console.log(this.listpoke);
       this.loading = false;
     },
       error => {
@@ -43,13 +54,12 @@ export class PokemonComponent implements OnInit {
           'content-Type': 'application/json',
         }
       }).subscribe(data => {
-        console.log(data.sprites.front_default);
         return data.sprites.front_default;
     },
       error => {
         return "https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/0.16.1/images/loader-large.gif";
     });
-    
+    ;
     
   }
 }
