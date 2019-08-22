f55d7e012857f677385946028ce5b6b653296606
berbernado <bernadoanthosoeryo@gmail.com>
2019-08-22 11:33:57 +0700

    2 files modified

--- src/app/view/pokemon/pokemon.component.ts
+++ src/app/view/pokemon/pokemon.component.ts
@@ -37,7 +37,19 @@ export class PokemonComponent implements OnInit {
   }
 
   getPokeImages(id){
-    let url = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/1.png";
-    return url;
+    this.httpService.get<any>(UrlCollection.FORMPOKEMON + id + '/',
+      {
+        headers:{
+          'content-Type': 'application/json',
+        }
+      }).subscribe(data => {
+        console.log(data);
+        return data.sprites.front_default;
+    },
+      error => {
+        return "https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/0.16.1/images/loader-large.gif";
+    });
+    
+    
   }
 }
