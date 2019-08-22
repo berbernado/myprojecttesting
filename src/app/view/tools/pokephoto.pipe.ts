ebf422cefefbe67ab618647185c942691b81d8a6
berbernado <bernadoanthosoeryo@gmail.com>
2019-08-23 00:13:37 +0700

    2 files modified

--- src/app/view/tools/pokephoto.pipe.ts
+++ src/app/view/tools/pokephoto.pipe.ts
@@ -1,6 +1,8 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Http } from '@angular/http';
+import { map } from 'rxjs/operators';
+import 'rxjs/add/operator/map';
 import { UrlCollection } from '../../../config/UrlCollection';
 @Pipe({
   name: 'GetPhoto',
@@ -11,25 +13,50 @@ export class GetPhoto implements PipeTransform {
   constructor(private httpService: HttpClient) { }
   
   transform(value: number, args?: any): any {
-    this.getPokeImages(value);
-    let url = this.getPokeImages(1);
-    console.log(url);
-    return ;
+    let url : any;
+    
+    
+
+    try {
+        url = this.getPhoto(value);
+        console.log(url);
+    } catch (e) {
+        // No content response..
+        console.log('error');
+    }
+    
+    
+    //this.getPokeImages(value);
+    return 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png';
+  }
+
+  getPhoto(id){
+    this.getPartners(id).then(data => {
+        console.log(data.sprites.front_default);
+        return data.sprites.front_default
+      });
+  }
+
+  getPartners(id) {
+    return new Promise(resolve => {
+      this.httpService.get(UrlCollection.FORMPOKEMON + id + '/')
+        .map(results => results)
+        .subscribe(data => {
+          this.getrul = data;
+          resolve(this.getrul);
+        });
+    });
   }
 
   getPokeImages(id){ 
-    let urlnya;
-    this.httpService.get<any>(UrlCollection.FORMPOKEMON + id + '/',
-      {
-        headers:{
-          'content-Type': 'application/json',
-        }
-      }).subscribe(
+    let url : any;
+    this.httpService.get<any>(UrlCollection.FORMPOKEMON + id + '/').subscribe(
         data => {
-          this.getrul = data.sprites.front_default;
+          url = data.sprites.front_default;
         },
           error => {
           console.log(error);
         });
+    console.log(url);
   }
 }
