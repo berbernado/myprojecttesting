3b11f9862525f463581f9afb64c1e29614ab301d
berbernado <bernadoanthosoeryo@gmail.com>
2019-08-22 11:19:02 +0700

    2 files modified, 1 added

--- src/app/view/view.module.ts
+++ src/app/view/view.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ContentComponent } from './content/content.component';
 import { PokemonComponent } from './pokemon/pokemon.component';
+import { FilterPipe} from '../../config/filter.pipe';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
-  declarations: [HeaderComponent, ContentComponent, FooterComponent, PokemonComponent],
+  declarations: [HeaderComponent, ContentComponent, FooterComponent, PokemonComponent, FilterPipe],
   exports: [ContentComponent]
 })
 export class ViewModule { }
