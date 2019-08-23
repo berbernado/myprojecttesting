5e36673daf0b6f287af4b487c0c678bff79e9daa
berbernado <bernadoanthosoeryo@gmail.com>
2019-08-23 21:13:51 +0700

    3 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ViewModule } from './view/view.module';
+import { ViewRoutingModule } from './app.routing';
 
 @NgModule({
   imports: [ 
     BrowserModule, 
     FormsModule,
     ViewModule,
-    HttpClientModule
+    HttpClientModule,
+    ViewRoutingModule
   ],
   declarations: [ 
     AppComponent,
